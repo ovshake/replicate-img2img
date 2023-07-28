1d9edce85b4eb0d7614e07b556bf6ca3eb8c3c69
Ubuntu <ubuntu@ip-172-31-47-243.ec2.internal>
2023-07-28 21:10:50 +0000

    add src, assets, requirements.txt

---
+++ src/main.py
@@ -0,0 +1,55 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor, Resize
+import requests
+from PIL import Image
+from io import BytesIO
+from diffusers import StableDiffusionImg2ImgPipeline, LMSDiscreteScheduler
+import os
+from urllib.parse import urlparse
+
+def initialize_pipeline(pipeline_model="nitrosocke/Ghibli-Diffusion", device="cuda"):
+    # Initialize pipeline
+    pipe = StableDiffusionImg2ImgPipeline.from_pretrained(pipeline_model,
+                                                          torch_dtype=torch.float16).to(device)
+    # Update scheduler
+    lms = LMSDiscreteScheduler.from_config(pipe.scheduler.config)
+    pipe.scheduler = lms
+
+    return pipe
+
+def generate_image(pipe, device="cuda", url_or_filepath=None, prompt=None,
+                   strength=0.75, guidance_scale=7.5, seed=1024):
+    if url_or_filepath is None or prompt is None:
+        raise ValueError("Both 'url_or_filepath' and 'prompt' must be provided.")
+
+    # Parse image from url or local file
+    parsed = urlparse(url_or_filepath)
+    if parsed.scheme in ['http', 'https']:
+        # Fetch image from url
+        response = requests.get(url_or_filepath)
+        init_image = Image.open(BytesIO(response.content)).convert("RGB")
+    else:
+        # Open image from local file
+        init_image = Image.open(url_or_filepath).convert("RGB")
+
+    init_image.thumbnail((768, 768))
+
+    # Set up random generator
+    generator = torch.Generator(device=device).manual_seed(seed)
+
+    # Generate initial image
+    image = pipe(prompt=prompt, image=init_image, strength=strength,
+                 guidance_scale=guidance_scale, generator=generator).images[0]
+
+    # Generate final image
+    image = pipe(prompt=prompt, image=init_image, strength=strength,
+                 guidance_scale=guidance_scale, generator=generator).images[0]
+
+    return image
+
+if __name__ == '__main__':
+    pipe = initialize_pipeline(pipeline_model="nitrosocke/Ghibli-Diffusion", device="cuda")
+    image = generate_image(pipe, device="cuda", url_or_filepath="assets/sketch-mountains-input.jpg", prompt="A van gogh painting of a starry night.")
+    image.save("assets/output.png")
+
